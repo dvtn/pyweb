053a2252d8f18519cd56be1d34af652489c20e82
david.tian <david.tian@live.com>
2020-03-23 21:19:40 +0800

    配置注册应用

--- django/wms/wms/settings.py
+++ django/wms/wms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'wms.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'wms',
+        'USER':'root',
+        'PASSWORD':'Love88me',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
